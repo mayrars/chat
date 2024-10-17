77905044100a65b781c8c720a31e992f4c6b23e6
mayrars <mayra203@gmail.com>
2024-10-17 08:51:46 -0600

    Created client and server

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import logger from 'morgan'
+
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+const port = process.env.PORT ?? 3000
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg)
+        io.emit('chat message', msg)
+    })
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd()+'/client/index.html')
+})
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+    
+})
