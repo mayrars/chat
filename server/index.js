a9ddcfb4a8b77c5fc1f786705346f1f669fd27dc
mayrars <mayra203@gmail.com>
2024-10-22 05:45:52 -0600

    Adding username and function to get username en SQL

--- server/index.js
+++ server/index.js
@@ -21,24 +21,45 @@ const db= createClient({
 await db.execute(`
     CREATE TABLE IF NOT EXISTS messages(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        content TEXT
+        content TEXT,
+        user TEXT
     )    
 `)
 
-io.on('connection', (socket) => {
+io.on('connection', async(socket) => {
     console.log('a user connected')
     socket.on('disconnect', () => {
         console.log('user disconnected')
     })
-    socket.on('chat message', (msg) => {
+    socket.on('chat message', async(msg) => {
         let result
+        let username = socket.handshake.auth.username ?? 'anonymus'
         try {
-            
+            result = await db.execute({
+                sql:`INSERT INTO messages (content, user) VALUES(:msg, :username)`,
+                args: { msg, username }
+            })
         }catch(e){
-
+            console.log(e)
+            return  
         }
-        io.emit('chat message', msg)
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
     })
+
+    if(!socket.recovered){
+        try {
+            const results = await db.execute({
+                sql: `SELECT id, content, user FROM messages WHERE id > ? `,
+                args: [socket.handshake.auth.serverOffset ?? 0]
+            })
+
+            results.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString(), row.user) 
+            });
+        }catch(e){
+            console.log(e)
+        }
+    }
 })
 
 app.get('/', (req, res) => {
